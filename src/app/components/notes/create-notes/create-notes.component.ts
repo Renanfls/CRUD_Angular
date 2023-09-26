972a35cc1bf71ba2ed147ecb50d7da3d8c64a490
Renanfls <contato.renanfabricio@gmail.com>
2023-09-26 03:39:10 -0300

    FEATURE: create-notes

--- src/app/components/notes/create-notes/create-notes.component.ts
+++ src/app/components/notes/create-notes/create-notes.component.ts
@@ -1,14 +1,49 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { lucideScrollText } from '@ng-icons/lucide';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
-
+import {
+  BrnRadioComponent,
+  BrnRadioGroupComponent,
+} from '@spartan-ng/ui-radiogroup-brain';
+import {
+  HlmRadioDirective,
+  HlmRadioGroupDirective,
+  HlmRadioIndicatorComponent,
+} from '@spartan-ng/ui-radiogroup-helm';
 @Component({
   selector: 'app-create-notes',
   standalone: true,
   imports: [
     HlmLabelDirective,
     HlmInputDirective,
+    BrnRadioComponent,
+    BrnRadioGroupComponent,
+    HlmRadioDirective,
+    HlmRadioGroupDirective,
+    HlmRadioIndicatorComponent,
+    NgIconComponent,
+    HlmButtonDirective,
+    FormsModule,
   ],
+  viewProviders: [provideIcons({ lucideScrollText })],
   templateUrl: './create-notes.component.html',
 })
-export class CreateNotesComponent {}
+export class CreateNotesComponent {
+  note = {
+    id: '1',
+    content: 'Teste',
+    subject: 'Dev',
+    model: 'model1',
+  };
+  createNote() {
+    alert('Nota criada!');
+  }
+
+  cancel() {
+    alert('Ação cancelada!');
+  }
+}
---
+++ src/libs/ui/ui-radiogroup-helm/README.md
@@ -0,0 +1,9 @@
+# src-libs-ui-ui-radiogroup-helm
+
+This library was generated with [Nx](https://nx.dev).
+
+
+## Running unit tests
+
+Run `nx test src-libs-ui-ui-radiogroup-helm` to execute the unit tests.
+
