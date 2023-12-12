23dc2af851c818f17cb4fd06ab7b4b8175e838c0
Renanfls <contato.renanfabricio@gmail.com>
2023-12-11 21:44:58 -0300

    Form reactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterModule],
+  imports: [
+    HeaderComponent, 
+    FooterComponent, 
+    RouterModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/components/notes/create-notes/create-notes.component.ts
+++ src/app/components/notes/create-notes/create-notes.component.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { lucideScrollText } from '@ng-icons/lucide';
@@ -39,19 +39,28 @@ import { NoteService } from '../note.service';
   viewProviders: [provideIcons({ lucideScrollText })],
   templateUrl: './create-notes.component.html',
 })
-export class CreateNotesComponent {
-  note: Note = {
-    content: '',
-    subject: '',
-    model: 'model1',
-  };
 
+
+export class CreateNotesComponent implements OnInit {
+
+  form!: FormGroup;
+  
   constructor(
     private service: NoteService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({ 
+      content: ['Formulário reativo'],
+      subject: ['Teste'],
+      model: ['model1'],
+    })
+  }
+
   createNote() {
-    this.service.create(this.note).subscribe(() => {
+    this.service.create(this.form.value).subscribe(() => {
       this.router.navigate(['/readNote']);
     });
   }
